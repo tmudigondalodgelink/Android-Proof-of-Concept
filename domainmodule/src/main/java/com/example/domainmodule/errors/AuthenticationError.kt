4d24e749bac8977ea6f6801adf9b3ba05fa0ed5e
Thanmayee Mudigonda <tmudigonda@lodgelink.com>
2025-03-20 14:13:02 -0600

    Updated syntax for AuthenticationError class

--- domainmodule/src/main/java/com/example/domainmodule/errors/AuthenticationError.kt
+++ domainmodule/src/main/java/com/example/domainmodule/errors/AuthenticationError.kt
@@ -1,9 +1,9 @@
 package com.example.domainmodule.errors
 
 sealed class AuthenticationError(message: String) : Throwable(message) {
-    class CredentialsParsingError(private val credentialsParsingError: CredentialsParsingError) : AuthenticationError(credentialsParsingError.message ?: "")
-    class DataError(private val dataError: com.example.domainmodule.errors.DataError.ParsingError) : AuthenticationError(dataError.message ?: "")
-    class UnexpectedError(private val errorMessage: String) : AuthenticationError(errorMessage)
+    class CredentialsParsingError(credentialsParsingError: com.example.domainmodule.errors.CredentialsParsingError) : AuthenticationError(credentialsParsingError.message ?: "")
+    class DataError(dataError: com.example.domainmodule.errors.DataError) : AuthenticationError(dataError.message ?: "")
+    class UnexpectedError(errorMessage: String) : AuthenticationError(errorMessage)
 
     val errorDescription: String?
         get() = message
