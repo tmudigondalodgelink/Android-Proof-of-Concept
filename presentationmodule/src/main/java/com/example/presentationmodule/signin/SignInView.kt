87aa49c1e2e9501cf63a8798762ce04973355dc9
Thanmayee Mudigonda <tmudigonda@lodgelink.com>
2025-03-25 13:55:35 -0600

    Updated SignInView UI and navigation

--- presentationmodule/src/main/java/com/example/presentationmodule/signin/SignInView.kt
+++ presentationmodule/src/main/java/com/example/presentationmodule/signin/SignInView.kt
@@ -1,34 +1,104 @@
 package com.example.presentationmodule.signin
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun SignInView(viewModel: ISignInViewModel = hiltViewModel<SignInViewModel>()) {
-    Column(
+fun SignInView(
+    viewModel: ISignInViewModel = hiltViewModel<SignInViewModel>(),
+    navigateOnSuccess: () -> Unit
+) {
+    Box(
         modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
     ) {
-        val email by viewModel.email.collectAsState()
-        val password by viewModel.password.collectAsState()
+        Column(
+            modifier = Modifier,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            val email by viewModel.email.collectAsState()
+            val password by viewModel.password.collectAsState()
 
-        TextField(value = email, onValueChange = {
-            viewModel.setEmail(it)
-        })
+            LaunchedEffect(viewModel.onSignInSuccess) {
+                if (viewModel.onSignInSuccess.value) {
+                    navigateOnSuccess()
+                }
+            }
 
-        TextField(value = password, onValueChange = {
-            viewModel.setPassword(it)
-        })
+            TextField(
+                value = email,
+                onValueChange = {
+                    viewModel.setEmail(it)
+                },
+                placeholder = {
+                    Text(
+                        text = "Email"
+                    )
+                },
+                colors = TextFieldDefaults.colors(
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent
+                ),
+                singleLine = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .border(1.dp, Color.Gray, RoundedCornerShape(8.dp))
 
-        Button(onClick = { viewModel.singIn() }) {
-            Text("Sign In")
+            )
+
+            TextField(
+                value = password,
+                onValueChange = {
+                    viewModel.setPassword(it)
+                },
+                placeholder = {
+                    Text(
+                        text = "Password"
+                    )
+                },
+                colors = TextFieldDefaults.colors(
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent
+                ),
+                singleLine = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .border(1.dp, Color.Gray, RoundedCornerShape(8.dp))
+
+            )
+
+            Button(
+                onClick = { viewModel.singIn() },
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text("Sign In")
+            }
         }
     }
 }
@@ -36,5 +106,5 @@ fun SignInView(viewModel: ISignInViewModel = hiltViewModel<SignInViewModel>()) {
 @Preview(showBackground = true)
 @Composable
 fun SignInView_Preview() {
-    SignInView(SignInViewModel.fake())
+    SignInView(SignInViewModel.fake(), {})
 }
--- presentationmodule/src/main/java/com/example/presentationmodule/signin/SignInViewModel.kt
+++ presentationmodule/src/main/java/com/example/presentationmodule/signin/SignInViewModel.kt
@@ -2,25 +2,19 @@ package com.example.presentationmodule.signin
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.domainmodule.errors.AuthenticationError
-import com.example.domainmodule.models.Email
-import com.example.domainmodule.models.ID
-import com.example.domainmodule.models.Name
-import com.example.domainmodule.models.User
 import com.example.domainmodule.usecases.FakeSignInUseCase
 import com.example.domainmodule.usecases.ISignInUseCase
 import com.example.domainmodule.utilities.FlowResult
 import com.example.presentationmodule.collectInScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 interface ISignInViewModel {
     val email: StateFlow<String>
     val password: StateFlow<String>
+    val onSignInSuccess: StateFlow<Boolean>
 
     fun setEmail(email: String)
     fun setPassword(password: String)
@@ -33,6 +27,7 @@ class SignInViewModel @Inject constructor(
 ): ViewModel(), ISignInViewModel {
     override val email: MutableStateFlow<String> = MutableStateFlow("")
     override val password: MutableStateFlow<String> = MutableStateFlow("")
+    override val onSignInSuccess: MutableStateFlow<Boolean> = MutableStateFlow(false)
 
     override fun setEmail(email: String) {
         this.email.value = email
@@ -46,7 +41,10 @@ class SignInViewModel @Inject constructor(
         signInUseCase.execute(email.value, password.value)
             .collectInScope(viewModelScope) { result ->
                 when (result) {
-                    is FlowResult.Success -> println("Signed in with ${result.value}")
+                    is FlowResult.Success -> {
+                        onSignInSuccess.value = true
+                        println("Signed in with ${result.value}")
+                    }
                     is FlowResult.Failure -> println("Error occurred: ${result.error}")
                 }
             }
